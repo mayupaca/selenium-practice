57817c51ca38bd67ad8a85adcf75450acf5615f1
Mayu Suzumura <47727645+mayupaca@users.noreply.github.com>
2024-04-15 08:33:01 -0400

    UI automation: google search

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import time
 
 service = Service(executable_path="chromedriver.exe")
@@ -7,7 +11,25 @@ options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 driver = webdriver.Chrome(service=service, options=options)
 
-
+# googleにアクセス
 driver.get("https://www.google.com")
+# 検索結果を表示して5秒後ウインドウを閉じる
+WebDriverWait(driver, 5).until(
+    EC.presence_of_element_located((By.CLASS_NAME, "gLFyf"))
+)
+input_element = driver.find_element(By.CLASS_NAME, "gLFyf")
+# google検索窓から"Selenium"を入力 + ENTERキー → 検索
+input_element.send_keys("Selenium" + Keys.ENTER)
+
+# 検索後、指定した結果をクリック＆遷移
+# LINK_TEXT = 完全一致
+# PARTIAL_LINK_TEXT = 一部一致
+WebDriverWait(driver, 5).until(
+    EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, "Selenium"))
+)
+link = driver.find_element(By.PARTIAL_LINK_TEXT, "Selenium")
+link.click()
+
+# 10秒後ウィンドウ閉じる
 time.sleep(10)
 driver.quit()
